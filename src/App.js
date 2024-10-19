00dbceb762678eb525d85eec22f854dfb5aee6ff
VivekNetha <90238482+Netha132@users.noreply.github.com>
2024-10-19 10:16:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bhishma Pitamaha</h1>
+    <img src="https://4.bp.blogspot.com/-P2B626iwguc/WVNDM1idZ9I/AAAAAAAABio/YQ23SI4yDGwBsLLxlI9B-07M2bp766VRwCLcBGAs/s1600/bhishma-pitamah.jpg" width="300px"  className="bishmaPic1"/>
+    <p>Bhishma also known as Pitamaha, Gangaputra, and Devavrata, was a prince, statesman and commander of ancient Indian Kuru kingdom and is a major character of the epic Mahabharata and the protagonist of the Bhishma Parva episode. He was the supreme commander of the Kaurava forces during the Kurukshetra War. He was the only character who witnessed the entirety of the events of the Mahabharata, beginning from the reign of his father, King Shantanu of the Kuru kingdom. Bhishma was the stepbrother of Vyasa, the grandfather of both the Pandavas and the Kauravas. He was a prominent statesman of the Kuru Kingdom. He was born as the youngest son of the King Shantanu and goddess Ganga.</p>
+    <img src="https://th.bing.com/th/id/R.0e61ba1099d9f2f4342795ec65f53643?rik=Kxln7pMJ%2baDioA&riu=http%3a%2f%2fpostoast.com%2fwp-content%2fuploads%2f2017%2f08%2fBhisma-Pitamah-sitting.jpg&ehk=ElfAzS4k2nQ1rDiR3CuS8j6crSoilOV01pw%2bq5avb8M%3d&risl=&pid=ImgRaw&r=0" width="300px" />
+    <h1>Affairs of Kuru Kingdom</h1>
+    <p>After the death of his father, Bhishma played a major role in the affairs of the Kuru kingdom. He managed the kingdom when there were succession crises. He also arranged the marriage of his nephews and tried to bring peace between his grand-nephews, the Kauravas and the Pandavas.
+
+        The text Harivamsa mentions that during the mourning period after Shantanu's death, Bhishma killed Ugrayudha Paurava, a statesman of Panchala kingdom who lusted for Satyavati and tried to buy her with wealth. According to the Mahabharata, Chitrangada was crowned as the king, however, he was soon killed by a Gandharva (celestial musician). Bhishma performed Chitrangada's funeral rites. Vichitravirya, who was too young to rule, was crowned as the king by Bhishma but the actual control of the kingdom was under Satyavati until he reached adulthood. Bhishma aided Satyavati during that time.</p>
+        <h1>Swayamvara of the Kashi princesses and battle with Parashurama</h1>
+        <img src="https://www.pakkapatriot.com/wp-content/uploads/2020/02/Bhishma-and-Parashurama-War.jpg" width="300px" />
+        <p>When Vichitravirya grew up, Bhishma decided to bring Amba, Ambika and Ambalika—the princesses of Kashi kingdom—and get them married to him. Bhishma reached the kingdom and fought Shalva, easily defeating him and rightfully won over the princesses, who were choosing their spouse in a Swayamvara (ceremony where a woman chooses her husband from a group of suitors either by holding a competition or a task and the winner marries the princess). Shalva, the ruler of Shalwa or Saubala Kingdom and the lover of Amba, attempted to stop Bhishma but failed. Upon reaching Hastinapura, Ambika and Ambalika consented to marry Vichitravirya, while Amba told Bhishma about her love for Shalva. Learning about her feelings, Bhishma sent Amba to Saubala Kingdom.</p>
+        <p>The Udyoga Parva further narrates about Amba as well as the battle between Bhishma and Parashurama. When Amba requested Shalva to marry her, he rejected her, claiming that he was already humiliated during the Svayamvara. He also told her that he could not accept a woman, who was won by another man. A variant suggests that after Amba returned to Hastinapur, Bhishma then asked Vichitravirya to marry her, but he also refused to marry her as she loved another man. With no one to accept her, Amba blamed Bhishma for her misery and wanted revenge from him. She went to the kings of several kingdom and tried to convince them to slay Bhishma; but none of them agreed. After she was advised by some sages, she met Parasurama, Bhishma's teacher and successfully convinced him in giving vow to help her.</p>
+    <h1>The Kurukshetra War </h1>
+    <img src="https://i.pinimg.com/originals/5b/a0/39/5ba039423d43dd413d48e9ea77341cff.jpg" alt="" />
+    <p>In the great battle at Kurukshetra, Bhishma was the supreme commander of the Kaurava forces for ten days. He fought reluctantly on the side of the Kauravas, having vowed to protect the city of Hastinapur from all threats and invasions.
+
+        Before the battle, at the Kauravas army's assembly, Bhishma singled out Karna and forbade him from participating in the war under his command, and even affronted him as Sutaputra (son of Suta - child of Kshatriya father and Brahmin mother). Karna then mutually returned the hostility by refusing to serve under Bhishma. In truth, both side of the affront were aware of Karna's true lineage, and had to make pretext to prevent Karna from battling his brothers.</p>
+        <p>Bhishma was one of the most powerful warriors of his time and in history. He acquired his prowess and invincibility from being the son of the sacred Ganga and by being a student of Lord Parashurama. Despite being about five generations old, Bhishma was too powerful to be defeated by any warrior alive at that time. Every day, he slew at least 10,000 soldiers and about a 1,000 rathas.
+
+            At the beginning of the war, Bhishma vowed not to kill any of the Pandavas, as he loved them, being their grand-uncle. Duryodhana often confronted Bhishma alleging that he was not actually fighting for the Kaurava camp as he would not kill any of the Pandavas. He also did not allow any of the Kauravas to be killed in the war, as he loved all his grand-nephews and wanted a peace negotiation.</p>
+        <h1>Death</h1>
+        <img src="https://4.bp.blogspot.com/-RKaDVMkRO78/WVNDM3ZByKI/AAAAAAAABis/kJ3PbdkacN4pYKtqXtvqsPVYx5FgYOFYQCEwYBhgL/s1600/bhishma-pitamah-on-the-bed-of-arrows.jpeg" width="300px" />
+        
+        <p>After the war, while on his deathbed (arrow bed), he gave deep and meaningful instructions to Yudhishthira on statesmanship and the duties of a king. Bhishma always gave priority to Dharma. He always walked in the path of Dharma, despite his state because of the vow, he was supposed to forcefully follow the orders of his king Dhritharashtra, which were mostly Adharma, he was totally upset. He was sure he must let dharma win and Pandavas win, but the way he led the war and stayed silent were his sins in a way and he paid for it with the bed of arrows. Finally, Bhishma gave up the fight, focusing his life force and breath, sealing the wounds, and waiting for the auspicious moment to give up his body on the arrow bed. He did wait for about 58 nights for the winter solstice or first day of Uttarayana to give up his body on the arrow bed. His body was later cremated by Emperor Yudhishthira in a great ceremony.</p>
+        <h1>Lord Krishna</h1>
+    <div className="krishna">
+        
+
+        <img src="https://i.pinimg.com/564x/72/2e/6e/722e6edbe209337d86631d98d2b69c30.jpg" className="krishna-pic"></img>
+        </div>
+    </div>
+  );
+}
+
+export default App;
